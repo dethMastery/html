9cbcda1d1c9d4cc56ea65c2f8f1c10316162caf2
Suphakit P <me@suphakit.net>
2023-06-12 02:14:53 +0700

    feat: about data listing content

---
+++ src/js/contents.js
@@ -0,0 +1,302 @@
+const bachelor = `Bachelor's degree`
+
+const data = {
+  about: `Hiya! I'm <a href="https://suphakit.net" target="_blank">Suphakit P.</a> (also known as <u>Joe</u>, <u>Joji</u>,
+<u>Joseph</u>, <u>Detzz</u>, <u>Dethz</u>, <u>Katsuragi</u>, etc. ). a kind of Full-Stack Dev. who love to make some Front-End Styling and love to making some music :)`,
+  schooling: [
+    {
+      degree: 'Kintergarden - Grade 6',
+      class: '',
+      school: 'Darasamutr Sriracha School',
+      year: '2005 - 2014'
+    },
+    {
+      degree: 'Grade 7-9',
+      class: 'MEP',
+      school: 'Sriracha School',
+      year: '2014 - 2018'
+    },
+    {
+      degree: 'Grade 10',
+      class: 'Sci-Math',
+      school: 'Piboonbumpen Demonstration School',
+      year: '2018 - 2019'
+    },
+    {
+      degree: 'Kintergarden - Grade 6',
+      class: 'Sci-Math',
+      school: 'Darasamutr Sriracha School',
+      year: '2019 - April 2021'
+    },
+    {
+      degree: bachelor,
+      class: 'Computer Engineering',
+      school: 'University of Phayao',
+      year: 'May 2021 - Present'
+    },
+  ],
+  techStats: [
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+      alt: 'javascript logo',
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+      alt: 'typescript logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
+      alt: 'bootstrap logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"',
+      alt: 'c logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+      alt: 'css3 logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+      alt: 'html5 logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+      alt: 'react logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg',
+      alt: 'vuejs logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg',
+      alt: 'svelte logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg',
+      alt: 'nuxtjs logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg',
+      alt: 'nextjs logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+      alt: 'express logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg',
+      alt: 'tailwindcss logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+      alt: 'docker logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+      alt: 'github logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+      alt: 'git logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
+      alt: 'mysql logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg',
+      alt: 'nginx logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg',
+      alt: 'npm logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+      alt: 'nodejs logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
+      alt: 'php logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/raspberrypi/raspberrypi-original.svg" height="40',
+      alt: 'raspberrypi logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ssh/ssh-original.svg',
+      alt: 'ssh logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/stylus/stylus-original.svg',
+      alt: 'stylus logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain.svg',
+      alt: 'ubuntu logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg',
+      alt: 'vscode logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg',
+      alt: 'vim logo'
+    },
+    {
+      img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg',
+      alt: 'linux logo'
+    },
+  ],
+  event: {
+    text: `This is every events I ever joined & every certificates I ever get.`,
+    events: [
+      {
+        title: 'TESA Top Gun Rally #16',
+        desp: 'A Competitve event @ Chitralada Technology Institute',
+        date: '5-10 Sept. 2022',
+        img: 'https://ksrc.suphakit.net/1662866532553/000001.jpg'
+      }
+    ],
+    certificates: [
+      {
+        title: 'TESA Top Gun Rally #16',
+        desp: 'Participated in TESA Top Gun Rally #16',
+        date: '5-10 Sept. 2022',
+        img: 'https://media.discordapp.net/attachments/967025462490591332/1107917398524297236/TESA-CERT.jpg'
+      }
+    ]
+  } 
+}
+
+const about = document.querySelector('#aboutText')
+
+const school = document.querySelector('#school')
+let schoolForData = ''
+data.schooling.forEach((schoolData) => {
+  const card = `<div class="card">
+  <div class="cardBody">
+    <h4 style="font-weight: 200;">
+      <b>
+        ${schoolData.degree}
+      </b>
+    </h4>
+    ${
+    (schoolData.class != '') ? `<p>${schoolData.class}</p>` : ''
+    }
+    <h3 style="font-weight: 300;">
+      ${schoolData.school}
+    </h3>
+  </div>
+  <div class="cardFooter">
+    ${schoolData.year}
+  </div>
+</div>`
+
+  if (schoolForData == '') {
+    schoolForData = card
+  } else {
+    schoolForData = schoolForData + card
+  }
+})
+
+const tech = document.querySelector('#techStats')
+let techForData = ''
+data.techStats.forEach((techData) => {
+  const techLogo = `<img src="${techData.img}" alt="${techData.alt}" class="techLogo" loading="lazy">`
+  
+  if (techForData == '') {
+    techForData = techLogo
+  } else {
+    techForData = techForData + techLogo
+  }
+})
+
+const events = document.querySelector('#eventsText')
+
+const joinedEvents = document.querySelector('#joinedEvents')
+let joinedForData = ''
+data.event.events.forEach((data) => {
+  const forData = `<div class="card">
+  <div class="cardHeader" style="background: url(${data.img});"></div>
+  <div class="cardBody">
+    <h4>
+      ${data.title}
+    </h4>
+    <span>
+      ${data.desp}
+    </span>
+  </div>
+  <div class="cardFooter">
+    ${data.date}
+  </div>
+</div>`
+
+  if (joinedForData == '') {
+    joinedForData = forData
+  } else {
+    joinedForData = joinedForData + forData
+  }
+})
+
+const certificates = document.querySelector('#certificates')
+let certForData = ''
+data.event.certificates.forEach((data) => {
+  const forData = `<div class="card">
+  <div class="cardHeader" style="background: url(${data.img});"></div>
+  <div class="cardBody">
+    <h4>
+      ${data.title}
+    </h4>
+    <span>
+      ${data.desp}
+    </span>
+  </div>
+  <div class="cardFooter">
+    ${data.date}
+  </div>
+</div>`
+
+  if (certForData == '') {
+    certForData = forData
+  } else {
+    certForData = certForData + forData
+  }
+})
+
+about.innerHTML = data.about
+school.innerHTML = `<div class="textContainer">
+  <h3 style="text-decoration: underline; font-weight: 300;">
+    My Educational Profile
+  </h3>
+  <div class="cardContainer">
+    ${schoolForData}
+  </div>
+</div>`
+tech.innerHTML = `<div class="textContainer">
+  <h3 style="text-decoration: underline; font-weight: 300;">
+    My technical stats
+  </h3>
+  <div class="techContainer">
+    ${techForData}
+  </div>
+</div>`
+
+events.innerHTML = data.event.text
+joinedEvents.innerHTML = `<div class="textContainer">
+  <h3 style="text-decoration: underline; font-weight: 300;">
+    Joined Events
+  </h3>
+  <div class="cardContainer">
+    ${joinedForData}
+  </div>
+</div>`
+certificates.innerHTML = `<div class="textContainer">
+  <h3 style="text-decoration: underline; font-weight: 300;">
+    Certificates
+  </h3>
+  <div class="cardContainer">
+    ${certForData}
+  </div>
+</div>`
